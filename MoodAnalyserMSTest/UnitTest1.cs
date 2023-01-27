a9c2efbb345ce67bb62c31a43b15af3125d1dca7
piyush-shaw <piyushshaw0000@gmail.com>
2023-01-27 19:23:06 +0530

    [Piyush] added to give a message abitility to analyse and response happy or sad mood

---
+++ MoodAnalyserMSTest/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using MoodAnalyserProblem;
+
+namespace MoodAnalyserMSTest;
+
+[TestClass]
+public class UnitTest1
+{
+    //Method to test sad message 
+    [TestMethod]
+    public void AnalyseMood_ShouldReturnSad()
+    {
+        //Arrange
+        string msg = "I am in Sad Mood";
+        string expected = "sad";
+        MoodAnalyser mood = new MoodAnalyser(msg);
+
+        //Act
+        string actual = mood.AnalyseMood();
+
+        //Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    //Method to test happy message
+    public void AnalyseMood_ShouldReturnHappy()
+    {
+        //Arrange
+        string msg = "I am in Any Mood";
+        string expected = "happy";
+        MoodAnalyser mood = new MoodAnalyser(msg);
+
+        //Act
+        string actual = mood.AnalyseMood();
+
+        //Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ MoodAnalyserProblem/MoodAnalyser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace MoodAnalyserProblem
+{
+	public class MoodAnalyser
+	{
+		public string message;
+
+		//constructor to initialize message
+		public MoodAnalyser(string message)
+		{
+			this.message = message;
+		}
+
+		//Method to analyse the mood from the given message
+		public string AnalyseMood()
+		{
+			if (message.ToLower().Contains("sad"))
+				return "sad";
+			else
+				return "happy";
+		}
+	}
+}
+
